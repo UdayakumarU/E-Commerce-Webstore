046a2adbd3c3ec9b897b43bedda888698210ac16
UdayakumarU <31164995+UdayakumarU@users.noreply.github.com>
2018-11-20 17:46:25 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+$mysql_hostname = "host";
+$mysql_user = "user";
+$mysql_password = "password$";
+$mysql_database = "database";
+$db = @mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+mysql_select_db($mysql_database) or die("Could not select database");
+
+?>
